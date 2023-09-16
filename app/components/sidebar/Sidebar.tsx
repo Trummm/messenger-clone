2bc6a564492274aa9a87c49537eedf6d3352433f
Trummm <trungpro2000.tp2@gmail.com>
2023-09-17 00:03:25 +0700

    update feature navbar

--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -1,15 +1,16 @@
 // import getCurrentUser from '@/app/actions/getCurrentUser'
 
-// import DesktopSidebar from './DesktopSidebar'
-// import MobileFooter from './MobileFooter'
+import getCurrentUser from '@/app/actions/getCurrentUser'
+import DesktopSidebar from './DesktopSidebar'
+import MobileFooter from './MobileFooter'
 
 async function Sidebar({ children }: { children: React.ReactNode }) {
-  // const currentUser = await getCurrentUser()
+  const currentUser = await getCurrentUser()
 
   return (
     <div className='h-full'>
-      {/* <DesktopSidebar currentUser={currentUser!} /> */}
-      {/* <MobileFooter /> */}
+      <DesktopSidebar currentUser={currentUser!} />
+      <MobileFooter />
       <main className='lg:pl-20 h-full'>{children}</main>
     </div>
   )
