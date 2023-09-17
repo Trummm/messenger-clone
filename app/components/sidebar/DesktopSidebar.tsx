9e5ac06f6e0d3772fe7bf733a32c29e4c4dfcedd
Trummm <trungpro2000.tp2@gmail.com>
2023-09-17 16:16:16 +0700

    users screen and conversation screen

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -14,8 +14,6 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
   const routes = useRoutes()
   const [isOpen, setIsOpen] = useState(false)
 
-  console.log({ currentUser }, 'TEST')
-
   return (
     <>
       <div
